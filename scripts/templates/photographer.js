b76940a1d0ffa3594e41c33a0f1c2d8bc5bc0b9b
tgodfreyarul <scocheflivegames@hotmail.fr>
2024-10-18 16:23:33 +0200

    🎨 Session Mento. 18/10/24

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,51 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+export function photographerTemplate(data) {
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const card = document.createElement('figure');
+        card.classList.add('card-index');
+
+        const link = document.createElement('a');
+        link.classList.add('link');
+        link.setAttribute("href", `./photographer.html?id=${id}`);
+
+        const description = document.createElement('div');
+        description.classList.add('description');
+
+        const imgContainer = document.createElement('div');
+        imgContainer.classList.add('img-container');
+
+        const img = document.createElement('img');
+        img.classList.add('portrait');
+        img.setAttribute("src", picture);
+
+        const h2 = document.createElement('h2');
+        h2.classList.add('name');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+
+        const location = document.createElement('p');
+        location.classList.add('location');
+        location.textContent = `${city}, ${country}`;
+
+        const taglineElement = document.createElement('p');
+        taglineElement.classList.add('tagline');
+        taglineElement.textContent = tagline;
+
+        const priceElement = document.createElement('p');
+        priceElement.classList.add('price');
+        priceElement.textContent = `${price}€/jour`;
+
+        link.appendChild(img);
+        link.appendChild(h2);
+        description.appendChild(location);
+        description.appendChild(taglineElement);
+        description.appendChild(priceElement);
+        card.appendChild(link);
+        card.appendChild(description);
+
+        return card;
     }
-    return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 }
